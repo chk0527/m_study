c401c9859700610e7bfe987f20f2e5048c20f0f7
chk <chlgusrb0213@navere.com>
2024-02-27 18:48:09 +0900

    1

---
+++ src/Admin.java
@@ -0,0 +1,17 @@
+public class Admin{
+    String adminID;
+    String adminPwd;
+
+    public Admin(String adminID, String adminPwd){
+        this.adminID = adminID;
+        this.adminPwd = adminPwd;
+    }
+
+    public String getAdminID() {
+        return adminID;
+    }
+    public String getAdminPwd() {
+        return adminPwd;
+    }
+
+}
